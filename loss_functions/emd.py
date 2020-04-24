abd38c069ad85db7781b1da3c4e7c54de680e2e0
Benjamin Jin <benjaminmjin@gmail.com>
2020-04-24 19:39:47 +0200

    Fix emd^2 calculation

--- loss_functions/emd.py
+++ loss_functions/emd.py
@@ -5,4 +5,4 @@ def earth_mover_distance(
         y_true: K.placeholder,
         y_pred: K.placeholder
 ) -> K.placeholder:
-    return K.sum(K.square(K.cumsum(y_pred) - K.cumsum(y_true)))
+    return K.sum(K.square(K.cumsum(y_true, axis=-1) - K.cumsum(y_pred, axis=-1)), axis=-1)
