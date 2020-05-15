856730d4ad3378b983656a077a3940823e2706d9
Benjamin Jin <benjaminmjin@gmail.com>
2020-05-15 18:13:41 +0200

    Add logging and improved checkpointing

--- evaluation/adience.py
+++ evaluation/adience.py
@@ -4,10 +4,13 @@ from typing import List, Callable
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import TensorBoard
 
 from data_handlers.adience import get_adience_info, ADIENCE_TRAIN_FOLDS_INFO_FILES, \
     ADIENCE_VALIDATION_FOLDS_INFO_FILES, ADIENCE_CLASSES
+from data_handlers.data_set_info import DatasetName
 from data_handlers.generators import get_generators
+from evaluation.logging import get_checkpoint_file, get_tensorboard_callback
 from models.alx import get_alxs
 from models.constants import LEARNING_RATES
 from models.vgg import get_model
@@ -26,7 +29,15 @@ def evaluate_adience_vgg(
             number_of_classes=len(ADIENCE_CLASSES)
         )
         evaluate_adience_folds(
-            model=model
+            model=model,
+            checkpoint_callback=get_checkpoint_file(
+                data_set_name=DatasetName.ADIENCE,
+                learning_rate=learning_rate
+            ),
+            tensorboard_callback=get_tensorboard_callback(
+                data_set_name=DatasetName.ADIENCE,
+                learning_rate=learning_rate
+            )
         )
 
 
@@ -40,24 +51,38 @@ def evaluate_adience_alxs(
             number_of_classes=len(ADIENCE_CLASSES)
         )
         evaluate_adience_folds(
-            model=model
+            model=model,
+            checkpoint_callback=get_checkpoint_file(
+                data_set_name=DatasetName.ADIENCE,
+                learning_rate=learning_rate
+            ),
+            tensorboard_callback=get_tensorboard_callback(
+                data_set_name=DatasetName.ADIENCE,
+                learning_rate=learning_rate
+            )
         )
 
 
 def evaluate_adience_folds(
-        model: Model
+        model: Model,
+        checkpoint_callback: ModelCheckpoint,
+        tensorboard_callback: TensorBoard
 ) -> None:
     evaluate_adience_fold(
         model=model,
         train_fold_info_files=ADIENCE_TRAIN_FOLDS_INFO_FILES[0],
-        validation_fold_info_file=ADIENCE_VALIDATION_FOLDS_INFO_FILES[0]
+        validation_fold_info_file=ADIENCE_VALIDATION_FOLDS_INFO_FILES[0],
+        checkpoint_callback=checkpoint_callback,
+        tensorboard_callback=tensorboard_callback
     )
 
 
 def evaluate_adience_fold(
         model: Model,
         train_fold_info_files: List[Path],
-        validation_fold_info_file: Path
+        validation_fold_info_file: Path,
+        checkpoint_callback: ModelCheckpoint,
+        tensorboard_callback: TensorBoard
 ) -> None:
     train_info, validation_info = get_adience_info(
         train_fold_info_files=train_fold_info_files,
@@ -71,7 +96,8 @@ def evaluate_adience_fold(
         model=model,
         train_generator=train_generator,
         validation_generator=validation_generator,
-        checkpoint_file=ADIENCE_CHECKPOINT_FILE
+        checkpoint_callback=checkpoint_callback,
+        tensorboard_callback=tensorboard_callback
     )
 
 
@@ -79,19 +105,12 @@ def evaluate(
         model: Model,
         train_generator: ImageDataGenerator,
         validation_generator: ImageDataGenerator,
-        checkpoint_file: Path
+        checkpoint_callback: ModelCheckpoint,
+        tensorboard_callback: TensorBoard
 ) -> None:
     model.fit(
         train_generator,
-        epochs=20,
+        epochs=1,
         validation_data=validation_generator,
-        callbacks=[
-            ModelCheckpoint(
-                filepath=str(checkpoint_file),
-                monitor='val_acc',
-                verbose=1,
-                save_best_only=True,
-                mode='max'
-            )
-        ]
+        callbacks=[checkpoint_callback, tensorboard_callback]
     )
---
+++ evaluation/logging.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from pathlib import Path
+
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+
+from data_handlers.data_set_info import DatasetName
+
+LOG_DIR = Path('./logs')
+CHECKPOINT_DIR = Path('./checkpoints')
+
+
+def get_tensorboard_callback(
+        data_set_name: DatasetName,
+        learning_rate: float
+) -> TensorBoard:
+    current_datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
+    data_set_log_dir = LOG_DIR / Path(data_set_name.value) / Path(str(learning_rate)) / Path(current_datetime)
+    data_set_log_dir.mkdir(parents=True, exist_ok=True)
+    return TensorBoard(
+        log_dir=data_set_log_dir
+    )
+
+
+def get_checkpoint_file(
+        data_set_name: DatasetName,
+        learning_rate: float
+) -> ModelCheckpoint:
+    current_datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
+    data_set_checkpoint_dir = CHECKPOINT_DIR / Path(data_set_name.value) / Path(str(learning_rate))
+    data_set_checkpoint_dir.mkdir(parents=True, exist_ok=True)
+    return ModelCheckpoint(
+        filepath=str(data_set_checkpoint_dir / Path(current_datetime)),
+        monitor='val_acc',
+        verbose=1,
+        save_best_only=True,
+        mode='max'
+    )
