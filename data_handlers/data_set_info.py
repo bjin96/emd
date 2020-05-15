c962d3414208e1ce2edbd70ab233dba4f597cee2
Benjamin Jin <benjaminmjin@gmail.com>
2020-05-15 18:00:57 +0200

    Add enum for dataset names

---
+++ data_handlers/data_set_info.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class DatasetName(Enum):
+    ADIENCE = 'adience'
+    IMAGES_OF_GROUPS = 'images_of_groups'
+    IMAGE_AESTHETICS = 'image_aesthetics'
+    IMAGE_NET = 'image_net'
