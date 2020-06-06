58cee9d00d8d83498992ea747c080425e187823b
Benjamin Jin <benjaminmjin@gmail.com>
2020-06-06 12:45:14 +0200

    Limit GPU memory usage

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from tensorflow.keras.losses import categorical_crossentropy
 
 from evaluation.adience import evaluate_adience_vgg_f, evaluate_adience_alxs, evaluate_adience_res
+import tensorflow as tf
+
+
+devices = tf.config.experimental.list_physical_devices('GPU')
+if devices:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            devices[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)]
+        )
+    except RuntimeError as e:
+        print(e)
 
 
 def main():
