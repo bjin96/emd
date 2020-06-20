658b370b7d176e8f2cf84cd56d558a44f4753b7e
Benjamin Jin <benjaminmjin@gmail.com>
2020-06-20 14:44:50 +0200

    Fix using head start for self-guided emd

--- evaluation/adience.py
+++ evaluation/adience.py
@@ -3,14 +3,11 @@ from typing import List, Callable, Union, Type
 
 from tensorflow.keras.activations import linear, softmax
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.callbacks import TensorBoard
 
 from data_handlers.adience import get_adience_info, ADIENCE_TRAIN_FOLDS_INFO_FILES, \
     ADIENCE_VALIDATION_FOLDS_INFO_FILES, ADIENCE_CLASSES
 from data_handlers.data_set_info import DatasetName
 from data_handlers.generators import get_generators
-from evaluation.logging import get_checkpoint_file, get_tensorboard_callback
 from models.constants import LEARNING_RATES
 from models.evaluation_model import EvaluationModel
 
@@ -24,44 +21,31 @@ def evaluate_adience_model(
     for learning_rate in LEARNING_RATES:
         model = evaluation_model(
             number_of_classes=len(ADIENCE_CLASSES),
+            dataset_name=DatasetName.ADIENCE,
             final_activation=final_activation,
             loss_function=loss_function,
             learning_rate=learning_rate,
             **loss_function_kwargs
         )
         evaluate_adience_folds(
-            model=model,
-            checkpoint_callback=get_checkpoint_file(
-                data_set_name=DatasetName.ADIENCE,
-                learning_rate=learning_rate
-            ),
-            tensorboard_callback=get_tensorboard_callback(
-                data_set_name=DatasetName.ADIENCE,
-                learning_rate=learning_rate
-            )
+            model=model
         )
 
 
 def evaluate_adience_folds(
-        model: EvaluationModel,
-        checkpoint_callback: ModelCheckpoint,
-        tensorboard_callback: TensorBoard
+        model: EvaluationModel
 ) -> None:
     evaluate_adience_fold(
         model=model,
         train_fold_info_files=ADIENCE_TRAIN_FOLDS_INFO_FILES[0],
-        validation_fold_info_file=ADIENCE_VALIDATION_FOLDS_INFO_FILES[0],
-        checkpoint_callback=checkpoint_callback,
-        tensorboard_callback=tensorboard_callback
+        validation_fold_info_file=ADIENCE_VALIDATION_FOLDS_INFO_FILES[0]
     )
 
 
 def evaluate_adience_fold(
         model: EvaluationModel,
         train_fold_info_files: List[Path],
-        validation_fold_info_file: Path,
-        checkpoint_callback: ModelCheckpoint,
-        tensorboard_callback: TensorBoard
+        validation_fold_info_file: Path
 ) -> None:
     train_info, validation_info = get_adience_info(
         train_fold_info_files=train_fold_info_files,
@@ -74,22 +58,17 @@ def evaluate_adience_fold(
     evaluate(
         model=model,
         train_generator=train_generator,
-        validation_generator=validation_generator,
-        checkpoint_callback=checkpoint_callback,
-        tensorboard_callback=tensorboard_callback
+        validation_generator=validation_generator
     )
 
 
 def evaluate(
         model: EvaluationModel,
         train_generator: ImageDataGenerator,
-        validation_generator: ImageDataGenerator,
-        checkpoint_callback: ModelCheckpoint,
-        tensorboard_callback: TensorBoard
+        validation_generator: ImageDataGenerator
 ) -> None:
     model.fit(
         x=train_generator,
         epochs=50,
         validation_data=validation_generator,
-        callbacks=[checkpoint_callback, tensorboard_callback]
     )
--- models/evaluation_model.py
+++ models/evaluation_model.py
@@ -7,6 +7,8 @@ from tensorflow.keras.activations import linear, softmax
 from tensorflow.keras.metrics import categorical_accuracy
 from tensorflow.keras.optimizers import SGD, Optimizer
 
+from data_handlers.data_set_info import DatasetName
+from evaluation.logging import get_checkpoint_file, get_tensorboard_callback
 from loss_functions.emd import EmdWeightHeadStart
 from metrics.accuracy import one_off_accuracy
 
@@ -24,18 +26,21 @@ class EvaluationModel(ABC):
     def __init__(
             self,
             number_of_classes: int,
+            dataset_name: DatasetName,
             final_activation: Union[softmax, linear],
             loss_function: Callable,
             learning_rate: float,
             **loss_function_kwargs,
     ):
+        self.learning_rate = learning_rate
+        self.dataset_name = dataset_name
+
         self._build_model(
             number_of_classes=number_of_classes,
             final_activation=final_activation
         )
         self._compile_model(
             loss_function=loss_function,
-            learning_rate=learning_rate,
             **loss_function_kwargs
         )
 
@@ -53,17 +58,17 @@ class EvaluationModel(ABC):
     def _compile_model(
             self,
             loss_function: Callable,
-            learning_rate: float,
             **loss_function_kwargs
     ):
+        self.emd_weight_head_start = EmdWeightHeadStart()
         self.model.compile(
             loss=loss_function(
                 second_to_last_layer=self._get_second_to_last_layer(),
-                emd_weight_head_start=EmdWeightHeadStart(),
+                emd_weight_head_start=self.emd_weight_head_start,
                 **loss_function_kwargs
             ),
             optimizer=self._OPTIMIZER(
-                learning_rate=learning_rate,
+                learning_rate=self.learning_rate,
                 momentum=self._OPTIMIZER_MOMENTUM
             ),
             metrics=self._METRICS,
@@ -74,4 +79,17 @@ class EvaluationModel(ABC):
         return self.model.predict(**kwargs)
 
     def fit(self, **kwargs):
-        return self.model.fit(**kwargs)
+        return self.model.fit(
+            callbacks=[
+                self.emd_weight_head_start,
+                get_checkpoint_file(
+                    data_set_name=self.dataset_name,
+                    learning_rate=self.learning_rate
+                ),
+                get_tensorboard_callback(
+                    data_set_name=self.dataset_name,
+                    learning_rate=self.learning_rate
+                )
+            ],
+            **kwargs
+        )
