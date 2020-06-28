03ee015d505484ea285b1ffce6023ee55dfad8c7
Benjamin Jin <benjaminmjin@gmail.com>
2020-06-28 19:30:34 +0200

    Improve one-off accuracy implementation

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -1,13 +1,7 @@
-import numpy as np
+import tensorflow as tf
 
 
 def one_off_accuracy(y_true, y_pred):
-    true_indices = np.argmax(y_true, axis=-1)
-    pred_indices = np.argmax(y_pred, axis=-1)
-    hits = 0
-    for i, prediction in enumerate(pred_indices):
-        max_index = y_true.shape[-1] - 1
-        left_neighbor = 0 if true_indices[i] == 0 else true_indices[i] - 1
-        right_neighbor = max_index if true_indices[i] == max_index else true_indices[i] + 1
-        hits += 1 if true_indices[i] in [prediction, left_neighbor, right_neighbor] else 0
-    return hits / len(y_true)
+    true_indices = tf.argmax(y_true, axis=-1)
+    pred_indices = tf.argmax(y_pred, axis=-1)
+    return tf.reduce_sum(tf.cast(tf.abs(true_indices - pred_indices) <= 1, tf.int32))
