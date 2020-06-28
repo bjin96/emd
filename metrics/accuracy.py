231e695be3f698060a099f1a26ac8f8ed8ec6156
Benjamin Jin <benjaminmjin@gmail.com>
2020-06-28 21:13:10 +0200

    Add missing division to one-off accuracy

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -4,4 +4,4 @@ import tensorflow as tf
 def one_off_accuracy(y_true, y_pred):
     true_indices = tf.argmax(y_true, axis=-1)
     pred_indices = tf.argmax(y_pred, axis=-1)
-    return tf.reduce_sum(tf.cast(tf.abs(true_indices - pred_indices) <= 1, tf.int32))
+    return tf.reduce_sum(tf.cast(tf.abs(true_indices - pred_indices) <= 1, tf.int32)) / y_true.shape[0]
